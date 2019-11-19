44d1461c806448f10c18b92a5dc0a2100fb6d805
stm <stm@ableton.com>
2019-11-19 17:02:46 +0100

    Add jbox_get_boxpath() to mock

--- c74_mock_ui.h
+++ c74_mock_ui.h
@@ -7,22 +7,24 @@
 
 namespace c74 {
 namespace max {
-
-
 	using t_jbox = t_object;
 	using t_pxjbox = t_jbox;
-	
-	
+
+  static t_symbol* emptysymbol = gensym("");
+
 	MOCK_EXPORT void class_dspinitjbox(t_class* c) {}
-	
+
 	MOCK_EXPORT void class_attr_setstyle(t_class *c, const char *name) {}
 
 	MOCK_EXPORT void z_jbox_dsp_setup(t_pxjbox* x, long nsignals) {}
-	
+
 	MOCK_EXPORT void z_jbox_dsp_free(t_pxjbox* x) {}
 
 	MOCK_EXPORT t_max_err jbox_notify(t_jbox* b, t_symbol* s, t_symbol* msg, void* sender, void* data) {
 		return 0;
 	}
+  MOCK_EXPORT t_symbol* jbox_get_boxpath(t_pxjbox* b) {
+    return emptysymbol;
+  }
 
 }}
