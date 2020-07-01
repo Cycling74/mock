f393408b5f5c7d94999fbd09a3c1e066452fe2b1
Hidde de Jong <hiddedejong0@gmail.com>
2020-07-01 15:40:48 +0200

    add stub for jgraphics_rectangle_fill_fast

--- c74_mock.cpp
+++ c74_mock.cpp
@@ -246,6 +246,7 @@ namespace max {
     MOCK_EXPORT void jgraphics_line_draw_fast(t_jgraphics* g, double x1, double y1, double x2, double y2, double linewidth) {}
     MOCK_EXPORT void jgraphics_move_to(t_jgraphics* g, double x, double y) {}
     MOCK_EXPORT void jgraphics_rectangle_rounded(t_jgraphics* g, double x, double y, double width, double height, double ovalwidth, double ovalheight) {}
+    MOCK_EXPORT void jgraphics_rectangle_fill_fast(t_jgraphics* g, double x, double y, double width, double height) {}
     MOCK_EXPORT void jgraphics_fill(t_jgraphics* g) {}
     MOCK_EXPORT void jgraphics_stroke(t_jgraphics* g) {}
     MOCK_EXPORT void jgraphics_set_source_jrgba(t_jgraphics* g, t_jrgba* rgba) {}
